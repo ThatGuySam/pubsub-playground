08a21b88491ac1a50bf5fe1aef326bc75d5bc46a
Sam Carlton <ThatGuySam@users.noreply.github.com>
2022-01-11 11:27:08 -0600

    Update logger.js

--- netlify/functions/logger.js
+++ netlify/functions/logger.js
@@ -1,11 +1,15 @@
 // Just logs received requests
+
+// https://stackoverflow.com/a/56954397/1397641
+const pubSubAckResponse = {
+  statusCode: 204,
+  body: 'OK'
+}
+
 exports.handler = async (event, context) => {
   console.log('event', event)
 
   return {
-    statusCode: 200,
-    body: JSON.stringify({
-      event
-    })
+    ...pubSubAckResponse
   }
 }
